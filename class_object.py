01cd61d4bd2c37cd6b1d694595ad3dd9c8f69401
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-21 00:31:45 +0530

    class, object and init

--- class_object.py
+++ class_object.py
@@ -1,10 +1,17 @@
 class Student:
-    def __init__(self, fullname):
+    #default constructor
+    def __init__(self):
+        print("default constructor initiated")
+
+    #parameterized constructor
+    def __init__(self, fullname, marks):
         #we can also name self to any other name but we should necessarily have it because it is the reference object
-        print(self)
         self.name = fullname
+        self.marks = marks
 #python automatically creates an __init__() function which is called constructor
-s1 = Student("yash")
-s2 = Student("ajay")
-s3 = Student("gurpreet")
-print(s1.name, s2.name, s3.name)
+s1 = Student("yash", 13)
+print(s1.name, s1.marks)
+s2 = Student("ajay",14)
+print(s2.name, s2.marks)
+s3 = Student("gurpreet",15)
+print(s3.name, s3.marks)
