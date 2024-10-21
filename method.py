3366e8b77a59577205a373848909ca3d24aa8c6f
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-21 11:19:25 +0530

    methods in class

--- method.py
+++ method.py
@@ -1,10 +1,15 @@
 class Student:
-    def __init__(self):
+    def __init__(self, age):
+        self.age = age
         print("default constructor initiated")
 
     def hello(self):
-        print(f"hello {username}")
+        print(f"hello {username}, your age is {self.age}")
+    
+    def get_age(self):
+        return self.age
 
 username = str(input("Enter the username: "))
-s1 = Student()
+s1 = Student(12)
 s1.hello()
+print(s1.get_age())
